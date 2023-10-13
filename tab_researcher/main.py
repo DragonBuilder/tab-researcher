e6d5b9292f846b4018cad47d9bf6f66f86a57808
Aneesh Anil <aneesh@geektrust.in>
2023-10-13 10:18:09 +0530

    understand if user knows what to look for in a tab

--- tab_researcher/main.py
+++ tab_researcher/main.py
@@ -4,17 +4,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = OpenAI(temperature=0)
+llm = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-tools = load_tools(["serpapi"], llm=llm)
+# tools = load_tools(["serpapi"], llm=llm)
 
-agent = initialize_agent(
-    tools=tools,
-    llm=llm,
-    agent="zero-shot-react-description",
-    verbose=True
-)
+# agent = initialize_agent(
+#     tools=tools,
+#     llm=llm,
+#     agent="zero-shot-react-description",
+#     verbose=True
+# )
 
-result = agent.run("Tablets under Rs. 20000")
+# result = agent.run("Tablets under Rs.20000")
 
-print(result)
+# print(result)
+question = "Do you know what you are looking for in a Tablet phone? (Yes/No)"
+
+while True:
+    response = input(question + " ")
+    if response.lower().startswith('y'):
+        break
+    print("\nPlease answer 'Yes' or 'No'\n")
+
+
+
+
+# print(llm(prompt))

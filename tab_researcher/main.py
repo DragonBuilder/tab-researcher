dd2f607b0f4dbd7a76d590f9b5b6f377e7473f35
Aneesh Anil <aneesh@geektrust.in>
2023-10-14 23:41:16 +0530

    lists tablets under Rs 20,000

--- tab_researcher/main.py
+++ tab_researcher/main.py
@@ -1,31 +1,43 @@
-from langchain.llms import OpenAI
+from langchain.llms.openai import OpenAI
+# from langchain.chat_models import ChatOpenAI
 from langchain.agents import load_tools, initialize_agent
+import openai
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
-# tools = load_tools(["serpapi"], llm=llm)
-
-# agent = initialize_agent(
-#     tools=tools,
-#     llm=llm,
-#     agent="zero-shot-react-description",
-#     verbose=True
+# chat_completion = openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo",
+#     messages = [
+#         {
+#             "role": "user",
+#             "content": "Hello, how are you?"
+#         }
+#     ]
 # )
 
-# result = agent.run("Tablets under Rs.20000")
+# print(chat_completion)
 
-# print(result)
-question = "Do you know what you are looking for in a Tablet phone? (Yes/No)"
+# llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+llm = OpenAI(temperature=0)
 
-while True:
-    response = input(question + " ")
-    if response.lower().startswith('y'):
-        break
-    print("\nPlease answer 'Yes' or 'No'\n")
+# result = llm("Hello, how are you?")
 
+# print(type(result))
+
+
+tools = load_tools(["serpapi"], llm=llm)
+
+agent = initialize_agent(
+    tools=tools,
+    llm=llm,
+    agent="zero-shot-react-description",
+    verbose=True
+)
+
+result = agent.run("What tablet phones are available under Rs. 20,000? List the name and prices.")
+
+print(result)
 
 
 
